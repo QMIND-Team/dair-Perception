ca03a4f4cde3ddf402f208e1bdc635c607805baf
Sam Cantor <s.cantor@queensu.ca>
2020-01-27 10:18:17 -0500

    add facial detection and basic movement

---
+++ ROS/moving/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/qmind/ROS/moving/src
+# Build directory: /home/qmind/ROS/moving/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(move_forward_py)
--- ROS/sam-testing/src/read_image_py/src/read_image.py
+++ ROS/sam-testing/src/read_image_py/src/read_image.py
@@ -19,6 +19,8 @@ import cv2
 
 VERBOSE = False
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 def image_callback(msg):
     print("Received an image!")
     if VERBOSE:
@@ -29,9 +31,23 @@ def image_callback(msg):
     # image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
     image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
+    # Trying face detection
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    #draw rectangles
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image_np, (x,y), (x+w, y+h), (255,0,0), 2)
+        cv2.putText(image_np, "Face", (x,y), font, 1, (255,0,0),1)
+        if (x > 340):
+            cv2.putText(image_np, "Left", (x,y+35), font, 1, (255,0,0),1)
+        else:
+            cv2.putText(image_np, "Right", (x,y+35), font, 1, (255,0,0),1)
+
     cv2.imshow('cv_img', image_np)
     cv2.waitKey(0)
     cv2.imwrite('./photos/test.jpg', image_np)
+
 
 def main():
     rospy.init_node('image_listener')
@@ -39,6 +55,7 @@ def main():
     image_topic = "/raspicam_node/image/compressed"
     # Set up your subscriber and define its callback
     rospy.Subscriber(image_topic, CompressedImage, image_callback)
+    #rospy.Publisher("/output/image_raw/compressed", CompressedImage)
     # Spin until ctrl + c
     rospy.spin()
 
